234fc1dc2582df7ce13ef8f478f5bb6b7fbf7258
GreenHuedMama <101072904+GreenHuedMama@users.noreply.github.com>
2022-10-01 12:36:57 -0500

    added codesandbox info

--- src/Weather.js
+++ src/Weather.js
@@ -24,3 +24,69 @@ export default function Weather(props) {
         />
     );
 }
+
+export default function Weather() {
+    let weatherData = {
+      city: "New York",
+      temperature: 73,
+      date: "Monday 9:48",
+      description: "Cloudy",
+      humidity: 77,
+      wind: 4
+    };
+  
+    return (
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>Last updated: {weatherData.date}</li>
+            <li>{weatherData.description}</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong>{weatherData.temperature}</strong>
+                <span className="units">
+                  <a href="/">C</a> | <a href="/">F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
